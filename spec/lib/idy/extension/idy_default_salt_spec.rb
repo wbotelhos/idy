6b3e240ceda4972e12161049a55171f0ee00c9da
Washington Botelho <wbotelhos@gmail.com>
2022-04-05 20:00:11 -0300

    ref: uses the whole text but colons

--- spec/lib/idy/extension/idy_default_salt_spec.rb
+++ spec/lib/idy/extension/idy_default_salt_spec.rb
@@ -7,12 +7,12 @@ RSpec.describe '#idy_default_salt' do
         expect(described_class.idy_default_salt).to eq '12345678910'
       end
     end
-  end  
+  end
 
   describe Namespace::NsModel do
     context 'when model is namespaced into a module' do
       it 'does not take into account the namespace' do
-        expect(described_class.idy_default_salt).to eq '141913154512'
+        expect(described_class.idy_default_salt).to eq '141135191613514'
       end
     end
   end
